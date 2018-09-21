73b1f7719ea20be0cdbf62c4dc5af3557a79f478
Kirzus <arnaud.malanda@gmail.com>
2018-09-21 16:32:36 +0200

    [JS] Get todo user input and store it an array

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -3,4 +3,12 @@
 //     todo: []
 // }
 
-$("")
+var todos = [];
+
+//Get user todo input and store it in todos array
+$("input[type='text']").on("keypress", function(e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        todos.push($(this).val());
+    }
+});
