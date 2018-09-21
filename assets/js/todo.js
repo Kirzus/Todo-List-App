9c1d4a365cc0a01b7a2edf79033c6a1cb9501e96
Kirzus <arnaud.malanda@gmail.com>
2018-09-22 01:43:36 +0200

    [Update][JS][CSS] Input todo on pressing enter and reset input text bar + todo basic styling

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -7,8 +7,13 @@ var todos = [];
 
 //Get user todo input and store it in todos array
 $("input[type='text']").on("keypress", function(e) {
-    if (e.which == 13) {
+    if (e.which === 13) {
         e.preventDefault();
-        todos.push($(this).val());
+        while ($(this).val() !== "") {
+            todos.push($(this).val());
+            $("#list-todos").append("<div class='todo'><input type='checkbox'><label>" + $(this).val() + "</label><i class='fas fa-edit'></i><i class='fas fa-trash-alt'></i></div>");
+            $(this).val("");
+        }
     }
 });
+
