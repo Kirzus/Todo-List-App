32ae0f78195917a9409be2d956e38d5f2dc9ed48
Kirzus <arnaud.malanda@gmail.com>
2018-09-22 18:41:55 +0200

    [Update][JS][CSS] Line through on checked checkbox + overfow on list-todos

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -10,8 +10,8 @@ $("input[type='text']").on("keypress", function(e) {
     if (e.which === 13) {
         e.preventDefault();
         while ($(this).val() !== "") {
-            todos.push($(this).val());
-            $(".list-todos").append("<div class='todo'><input type='checkbox'><label>" + $(this).val() + "</label><i class='fas fa-edit'></i><i class='fas fa-trash-alt'></i></div>");
+            todos.unshift($(this).val());
+            $(".list-todos").prepend("<div class='todo'><input type='checkbox'><label>" + $(this).val() + "</label><i class='fas fa-edit'></i><i class='fas fa-trash-alt'></i></div>");
             $(".fa-edit").addClass("edit-todo");
             $(".fa-trash-alt").addClass("trash-todo");
             $(this).val("");
@@ -25,4 +25,13 @@ $(".list-todos").on("click", ".trash-todo", function() {
     console.log(todo);
     todos.splice(todo.index(), 1);
     todo.remove();
+});
+
+$(".list-todos").on("click", "input[type=checkbox]", function() {
+    var todo = $(this).parent();
+    if ($(this).prop("checked") !== false) {
+        todo.addClass("done");
+    } else {
+        todo.removeClass("done");
+    }
 });
