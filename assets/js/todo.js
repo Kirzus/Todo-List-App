f5d343dc27c005af87e55bb7836fdcf1a4722e90
Kirzus <arnaud.malanda@gmail.com>
2018-09-22 17:11:24 +0200

    [Update][JS] Added delete todo

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -11,9 +11,18 @@ $("input[type='text']").on("keypress", function(e) {
         e.preventDefault();
         while ($(this).val() !== "") {
             todos.push($(this).val());
-            $("#list-todos").append("<div class='todo'><input type='checkbox'><label>" + $(this).val() + "</label><i class='fas fa-edit'></i><i class='fas fa-trash-alt'></i></div>");
+            $(".list-todos").append("<div class='todo'><input type='checkbox'><label>" + $(this).val() + "</label><i class='fas fa-edit'></i><i class='fas fa-trash-alt'></i></div>");
+            $(".fa-edit").addClass("edit-todo");
+            $(".fa-trash-alt").addClass("trash-todo");
             $(this).val("");
         }
     }
 });
 
+//remove todo when clicking on the trash button
+$(".list-todos").on("click", ".trash-todo", function() {
+    var todo = $(this).parent();
+    console.log(todo);
+    todos.splice(todo.index(), 1);
+    todo.remove();
+});
