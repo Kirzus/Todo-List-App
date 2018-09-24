4f316ad8b104a525c3c469a0d158d21d1a051c2a
Kirzus <arnaud.malanda@gmail.com>
2018-09-24 17:57:20 +0200

    [Update][JS] Added Rename todo feature

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -61,7 +61,7 @@ function todoActions() {
     doneTodo();
     renameTodo();
     removeTodo();
-}
+} 
 
 function addTodo() {
 //Get user todo input and store it in todos array
@@ -84,32 +84,50 @@ function addTodo() {
         }
     });
 }
-
+console.log($(".todo-text").is(":focus"));
 
 function renameTodo() {
-    $(".todos-container").on("click", ".todo-edit", function(e) {
-        var todoTxt = $(this).siblings(".todo-text");
-        var txt = todoTxt.text();
-        console.log(txt);
-        $(todoTxt).replaceWith("<input type='text' class='todo-text'>")
-        todoTxt.val(txt);
-        console.log(todoTxt.val());
-        console.log(index(todoTxt));
+    $(".todos-container").on("click", ".todo-edit", function() {
+        // var focused = $(".todo-text").is(":focus");
+        // console.log(focused);
+        while ($("input.todo-text").is(":focus") === false) {
+            var todoTxt = $(this).siblings(".todo-text");
+            var txt = todoTxt.text();
+            // console.log(txt);
+            $(todoTxt).replaceWith("<input type='text' class='todo-text'>")
+            $("input.todo-text").focus();
+            $("input.todo-text").val(txt);
+            console.log($("input.todo-text").is(":focus"));
+        }
     });
-
-    // $(".todos-container").on("keypress", ".todo-text", function(e) {
-    //     if (e.which === 13) {
-    //         e.preventDefault();
-    //         while ($(this).val() !== "") {
-    //             todos.slice($(this))
-    //         }
-    //     }
-    // });
+    
+    $(".todos-container").on("blur", "input.todo-text", function() {
+        if ($(this).val() !== "") {
+            var todoIndex = $(this).parent().index();
+            todos[todoIndex] = $(this).val();
+            $(this).replaceWith("<label class='todo-text'>" + $(this).val() + "</label>");
+            console.log(todos);
+            console.log($("input.todo-text").is(":focus"));
+        }
+    });
+//When renaming save todo and get back to a <label>
+    $(".todos-container").on("keypress", "input.todo-text", function(e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            if ($(this).val() !== "") {
+                $(this).trigger("blur");
+                // var todoIndex = $(this).parent().index();
+                // todos[todoIndex] = $(this).val();
+                // $(this).replaceWith("<label class='todo-text'>" + $(this).val() + "</label>");
+                // console.log(todos);
+            }
+        }
+    });
 }
 
 
 function removeTodo() {
-    $(".todos-container").on("click", ".trash-todo", function() {
+    $(".todos-container").on("click", ".todo-delete", function() {
         //Select and store the current todo 
         var todo = $(this).parent();
         //Remove the todo & its data
